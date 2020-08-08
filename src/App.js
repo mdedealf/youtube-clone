b95adddeceb93d78679e29cf9ce4d31a35237eb8
Mdedealf <dede.alfaruqq@gmail.com>
2020-08-08 23:10:46 +0700

    adding header, sidebar, content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import RecommendedVideos from './RecommendedVideos';
 
 function App() {
 	return (
 		<div className="app">
-			<h1>App js</h1>
+			<Header />
+
+			<div className="app__page">
+				<Sidebar />
+				<RecommendedVideos />
+			</div>
 		</div>
 	);
 }
