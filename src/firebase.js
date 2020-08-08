d543eb51347d2cb7b15ec399db4bbf1723dca079
Mdedealf <dede.alfaruqq@gmail.com>
2020-08-08 18:44:19 +0700

    deleting some file doesnt need, and add new file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7in3O26XZJkV7wMJ5pBxpWlplEXue_4M",
+    authDomain: "clone-c4a1a.firebaseapp.com",
+    databaseURL: "https://clone-c4a1a.firebaseio.com",
+    projectId: "clone-c4a1a",
+    storageBucket: "clone-c4a1a.appspot.com",
+    messagingSenderId: "967076657758",
+    appId: "1:967076657758:web:6e6f6ae8bf3b630a0852cf",
+    measurementId: "G-FXQ6QVPJM4"
+  };
