02e4fcd08bbc91ab721fd1d9a26924154da4f198
Mdedealf <dede.alfaruqq@gmail.com>
2020-08-09 09:12:11 +0700

    adding new component in RecommendedVideos.js and new file VideoCard js & VideoCard css

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './RecommendedVideos.css';
+import VideoCard from './VideoCard';
 
 function RecommendedVideos() {
 	return (
 		<div className="recommendedVideos">
 			<h2>Recommended</h2>
+			<VideoCard />
 		</div>
 	);
 }
---
+++ src/VideoCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './VideoCard.css';
+
+function VideoCard() {
+	return (
+		<div>
+			<h1>aad</h1>
+		</div>
+	);
+}
+
+export default VideoCard;
