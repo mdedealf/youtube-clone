0e52bb3e77c28078b87e86a4f800fe75ddb94593
Mdedealf <dede.alfaruqq@gmail.com>
2020-08-09 10:51:23 +0700

    install react router, and add search page

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -6,22 +6,34 @@ import SearchIcon from '@material-ui/icons/Search';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import { Avatar } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 function Header() {
+	const [ inputSearch, setInputSearch ] = useState('');
+
 	return (
 		<div className="header">
 			<div className="header__left">
 				<MenuIcon />
+                <Link to={"/"}>
 				<img
 					className="header__logo"
 					src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
 					alt=""
 				/>
+                </Link>
 			</div>
 
 			<div className="header__input">
-				<input type="text" placeholder="Search" />
-				<SearchIcon className="header__inputButton" />
+				<input
+					onChange={(e) => setInputSearch(e.target.value)}
+					value={inputSearch}
+					type="text"
+					placeholder="Search"
+				/>
+				<Link to={`/search/${inputSearch}`}>
+					<SearchIcon className="header__inputButton" />
+				</Link>
 			</div>
 
 			<div className="header__icons">
---
+++ src/SearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SearchPage.css';
+
+function SearchPage() {
+	return (
+		<div>
+			<h1>asdasd</h1>
+		</div>
+	);
+}
+
+export default SearchPage;
